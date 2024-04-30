5ae891cfaab0ee3673c189fe100c92bb9ef77d9c
architbatham <71764919+architbatham@users.noreply.github.com>
2024-04-30 11:41:02 +0530

    j

--- router.js
+++ router.js
@@ -13,8 +13,8 @@ app.set("views", "./views");
 var urlRoute = "<h2> Click on link to redirect</h2> <a href = '/'>Home page</a><br/><br/> <a href = '/about'>About page</a><br/><br/> <a href = '/contact'>Contact page</a><br/><br/> <a href = '/service'>Service page</a><br/><br/> <a href = '/register'>Register page</a><br/><br/> <a href = '/login'>Login page</a><br/><br/> "
 
 app.get("/",(req,res)=>{
-    res.send("<h1>/ or /home Url invoked </h1>" + urlRoute)
+    res.send("<h1>/ or /home Url iiinvoked </h1>" + urlRoute)
 });
 
 app.listen(3000);
-console.log("Server invoked at link--> http://localhost:3000");
+console.log("Server invoked at link---> http://localhost:3000");
