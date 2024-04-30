14e40558f37cf2d0f1048cac089738c84020e68a
Mohit <mohitrathore8269@gmail.com>
2024-04-30 11:23:05 +0530

    done

--- router.js
+++ router.js
@@ -3,7 +3,12 @@ import url from 'url';//step 2 import url module for taking data
 
 const app = express()//top level function
 
-//Step 1 now we create router
+
+import homerouter from './router/home.router.js';
+app.use("/",homerouter);
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 var urlRoute = "<h2> Click on link to redirect</h2> <a href = '/'>Home page</a><br/><br/> <a href = '/about'>About page</a><br/><br/> <a href = '/contact'>Contact page</a><br/><br/> <a href = '/service'>Service page</a><br/><br/> <a href = '/register'>Register page</a><br/><br/> <a href = '/login'>Login page</a><br/><br/> "
 
--- router/home.router.js
+++ router/home.router.js
@@ -1,3 +1,10 @@
 import express from 'express';
 
-let 
+let router = express.Router();
+
+router.get("/", (req,res)=>{
+    res.render("home");
+})
+
+
+export default router
